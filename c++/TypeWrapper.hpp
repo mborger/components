01ad3a8c6d33da10c56575ec505ca5b2a94ac61e
Matthew Borger <matthew@borgernet.com>
2014-07-12 22:36:16 -0700

    Added a hash implementation for TypeWrapper.

--- c++/TypeWrapper.hpp
+++ c++/TypeWrapper.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <typeinfo>
+#include <ostream>
 
 /**
  * @brief A class to give meaning to common data types.
@@ -7,6 +8,8 @@
  * Often data types are shared for multiple purposes such as using longs
  * to represent ids for an object model. It can get confusing to be passing
  * around longs that might not represent an id for the model you're working with.
+ * You can create a new type ModelId which extends a TypeWrapper<long> to
+ * have more type safety over working with primitives.
  * The constructor can also be overriden to provide some validation.
  * The stored value is designed to be immutable.
  */
@@ -17,9 +20,14 @@ public:
 	/**
 	 * @brief Value initializing constructor
 	 */
-	TypeWrapper(const T& t) : value(t) {}
+	TypeWrapper(const T& t) :
+		value(t)
+	{}
 
-	const T& getValue() const { return value; }
+	const T& getValue() const
+	{
+		return value;
+	}
 
 	/**
 	 * The typeid is checked to prevent two differnet subclasses
@@ -28,8 +36,15 @@ public:
 	 * you probably don't want them to be equivalent even if the stored integer
 	 * value is the same.
 	 */
-	virtual bool operator==(const TypeWrapper& rhs) const {
+	virtual bool operator==(const TypeWrapper& rhs) const
+	{
 		return (typeid(*this)==typeid(rhs)) && (this->value==rhs.value);
+	}
+
+	friend std::ostream& operator<<(std::ostream &out, const TypeWrapper<T> &tw)
+	{
+		out << tw.value;
+		return out;
 	}
 
 protected:
@@ -38,9 +53,37 @@ protected:
 	/**
 	 * @brief Default constructor for validation
 	 *
-	 * This protected constructor exists so that a subclass may perform
+	 * This protected constructor exists so that a derived class may perform
 	 * validation on the given value before initializing the stored
-	 * value which may be an expensive copy.
+	 * value which may be a costly copy.
 	 */
 	TypeWrapper() {}
 };
+
+namespace std {
+
+/**
+ * @brief Standard hash function
+ *
+ * This hash function delegates to the standard
+ * hash function of the wrapped type. I made it a class
+ * instead of a struct so that derived classes of TypeWrapper
+ * can just do the following:
+ *
+ * @code
+ * namespace std {
+ * 	template<>
+ * 	class hash<Foo> : public hash<TypeWrapper<type>> {};
+ * }
+ * @endcode
+ */
+template<typename T>
+class hash<TypeWrapper<T>> {
+	public:
+	std::size_t operator()(const TypeWrapper<T>& t) const
+	{
+		return std::hash<T>()(t.getValue());
+	}
+};
+
+} //namespace std
